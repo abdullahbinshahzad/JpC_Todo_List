922fc6efca98018f3a209c1e25ff28aa4b15d903
abdullahbinshahzad <abdullahbinshahzad228@gmail.com>
2024-01-23 15:22:13 +0500

    Initial commit

---
+++ app/src/main/java/com/example/jpc_todo_list/feature_note/domain/model/Note.kt
@@ -0,0 +1,21 @@
+package com.example.jpc_todo_list.feature_note.domain.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.example.jpc_todo_list.ui.theme.BabyBlue
+import com.example.jpc_todo_list.ui.theme.LightBlue
+import com.example.jpc_todo_list.ui.theme.RedOrange
+import com.example.jpc_todo_list.ui.theme.Violet
+
+@Entity
+data class Note(
+    @PrimaryKey(autoGenerate = true) val id: Int,
+    val title: String,
+    val description: String,
+    val timeStamp: Long,
+    val color: String
+){
+    companion object{
+        val noteColor = listOf(RedOrange, Violet, BabyBlue, LightBlue)
+    }
+}
---
+++ app/src/main/java/com/example/jpc_todo_list/feature_note/domain/use_case/GetNotes.kt
@@ -0,0 +1,7 @@
+package com.example.jpc_todo_list.feature_note.domain.use_case
+
+import com.example.jpc_todo_list.feature_note.domain.repository.NoteRepository
+
+class GetNotes(private val noteRepository: NoteRepository) {
+
+}
